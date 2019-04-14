a7fe571eb931b8b3357d941ea8a2c2573bfe2760
Brandon Dias <brandon.dias@websis.com.br>
2019-04-14 20:22:44 -0300

    Fixing graphs bug. Adding Ramp input

--- rlocus_gain_up_script.m
+++ rlocus_gain_up_script.m
@@ -4,22 +4,27 @@
 
 % Cleaning Up
 close all; clear; clc;
-% setting object 's' in order to write intuitive functions
-s = zpk(0,[],1);
-LOADING_ = "."
+s = zpk(0,[],1); % setting object 's' in order to write intuitive functions
+ramp = 1/s;
+t = 0:0.1:20; % time stamp
 
 %-------------------------------------------------------------------------%
 %------------------------Setting Study Case-------------------------------%
 %-------------------------------------------------------------------------%
 
+IMPUT_SIGNAL_RAMP = true;
+
+
 % Specify an arbitrary transfer function
-G  = 1/((s+2)*(s+3)*(s+7));
+G_plant  = 1/((s)*(s+3)*(s+5));
 % Specify an arbitrary feedback transfer function
 H  = 1;
+% Specify the transfer function of your compensator
+G_compc = (s + 0.3)/(s+0.03)
 
 % Special parameters
-k   = 0; % Select Gain of interest
-OP_ = 10; % Overshoot Percentage (put as X %)
+k   = 27.9948; % Select Gain of interest
+OP_ = 0; % Overshoot Percentage (put as X %)
 C_  = 0; % Dumping Factor 0 < C_ < 1
 t_  = 0; % Time constant
 
@@ -29,36 +34,60 @@ t_  = 0; % Time constant
 
 if H == 1
     % % Generating Open Loop Locus-Root Diagram: 
-    % rlocus(G);
+    % rlocus(G_plant);
     % grid on;
     % figure;
 
-    % Applying feedback and Gain, if there is one
-    if ((k > 0) && (OP_ == 0))
-        G_f1 = feedback(G,k);
-    end
     % Searching for specifc Gain based on OP_value
     if ((k == 0) && (OP_ > 0))
         k = 1;
-        G_f1 = feedback(G,k);
-        get_step = stepinfo(G_f1);
-        while (get_step.Overshoot < OP_)
+        G_fb = feedback(G_plant,k);
+        G_fb_step = stepinfo(G_fb);
+        while (G_fb_step.Overshoot < OP_)
             k = k + 0.01;
-            G_f1 = feedback(G,k);
-            get_step = stepinfo(G_f1);
+            G_fb = feedback(G_plant,k);
+            G_fb_step = stepinfo(G_fb);
         end
         found_msg = "Found K = ";
         disp(found_msg);
         disp(k);
     end
-
-    % Step Response for Closed Loop tranfer function
-    step(G_f1);
-    get_step = stepinfo(G_f1);
+    
+    % Print Input
+    if IMPUT_SIGNAL_RAMP == true
+        step(ramp,t);
+        hold on;
+    end
+   
+    % Step Response for Closed Loop tranfer function, no compensation
+    if ((k > 0) && (OP_ == 0))
+        G_plant = G_plant*k
+        G_fb = feedback(G_plant,1)
+    end
+    if IMPUT_SIGNAL_RAMP == true
+        G_fb = G_fb*ramp;
+    end
+    step(G_fb);
     grid on;
+    hold on;
+      
+    % Step Response for Closed Loop Compensator
+    Gc = G_compc * G_plant;
+    Gc_fb = feedback(Gc,1)
+    if IMPUT_SIGNAL_RAMP == true
+        Gc_fb = Gc_fb*ramp;
+    end
+    step(Gc_fb);
+    hold on;
     
     %Print information 
-    get_step
+    if IMPUT_SIGNAL_RAMP == false
+        G_fb_step = stepinfo(G_fb);
+        Gc_fb_step = stepinfo(Gc_fb);
+        G_fb_step 
+        Gc_fb_step
+    end
+    
 end
 
 
